d09718400be4f78bee953b4ec0a76b4c990298fd
sjodi <emil.vianden@gmail.com>
2026-05-29 22:59:46 +0200

    :3c

---
+++ Shitfuck.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public partial class Shitfuck : CharacterBody3D
+{
+	public const float Speed = 5.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector3 direction = new Vector3(inputDir.X, 0, inputDir.Y).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
